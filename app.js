723eb5d26778ce0ddb12db3b90ca7da4e67d051f
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-03-28 22:01:26 +0700

    feat: add starting task function

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 // Place your code here
 // Add any additional code necessary to fulfill the requirements of the assignment
+
+const getTask = async () => {
+    const response = await fetch("https://module3-api-is2m.onrender.com/random-todos");
+    const data = await response.json();
+
+    taskData = data;
+    // console.log('task data', taskData);
+
+    if (taskData.length > 0) {
+        const taskList = document.getElementById("taskList");
+        let list = '';
+        for (let i = 0; i < taskData.length; i++) {
+            list = list + `<li id='list-${i}'>${taskData[i]}</li>`;
+            // console.log(list);
+        }
+        taskList.innerHTML = list;
+    }
+};
+
+let taskData = [];
+
+getTask();
