a021a9da0295f4806c2a4448d8f159930f9b3c62
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-03-29 10:49:04 +0700

    chore: add comments

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // Place your code here
 // Add any additional code necessary to fulfill the requirements of the assignment
 
+// a universal variable taskId to keep track of the task-id so there are no task that has the same id after adding/removing
 let taskId = 0;
 
+// generate random task from api fetch from the start
 const generateTask = async () => {
     const response = await fetch("https://module3-api-is2m.onrender.com/random-todos");
     const data = await response.json();
@@ -13,6 +15,7 @@ const generateTask = async () => {
     }
 };
 
+// function to add a new task
 const addTask = () => {
     const newTask = document.getElementById("newTask");
     const taskList = document.getElementById("taskList");
@@ -21,7 +24,7 @@ const addTask = () => {
     if (isInput) {
         //  .insertAdjacentHTML is used to preserve the event listener 
         //  ref=> https://stackoverflow.com/questions/595808/is-it-possible-to-append-to-innerhtml-without-destroying-descendants-event-list
-        taskList.insertAdjacentHTML("beforeend", `<li id='list-${++taskId}'>${newTask.value}</li>`);
+        taskList.insertAdjacentHTML("beforeend", `<li id='task-${++taskId}'>${newTask.value}</li>`);
         newTask.value = '';
         showWarning(isInput);
         addClickListener(taskId);
@@ -30,29 +33,33 @@ const addTask = () => {
     }
 };
 
+// function to add an eventListener on click to the task
 const addClickListener = (index) => {
     let onClickTaskGenerator = onClickTask(index);
-    document.getElementById(`list-${index}`).addEventListener("click", () => onClickTaskGenerator.next());
+    document.getElementById(`task-${index}`).addEventListener("click", () => onClickTaskGenerator.next());
 };
 
+// generator function so the task can be clicked twice, first: after the task complete, second: remove the task
 function* onClickTask(index) {
-    const clickedTask = document.getElementById(`list-${index}`);
+    const clickedTask = document.getElementById(`task-${index}`);
     yield clickedTask.classList.add("task-done");
     
     clickedTask.remove();
 }
 
+// function to show the first generated task from api fetch
 const showTaskList = (taskData) => {
     const taskList = document.getElementById("taskList");
 
     for (let i = 0; i < taskData.length; i++) {
         const task = taskData[i];
         taskId = i;
-        taskList.insertAdjacentHTML("beforeend", `<li id='list-${taskId}'>${task}</li>`);
+        taskList.insertAdjacentHTML("beforeend", `<li id='task-${taskId}'>${task}</li>`);
         addClickListener(i);
     }
 };
 
+// function to show warning text if there is no input
 const showWarning = (valid) => {
     if (!valid) {
         warningText.innerHTML = 'Input a Task!';
@@ -61,8 +68,12 @@ const showWarning = (valid) => {
     }
 }
 
+// create a p tag to show warning text if there is no input
 const warningText = document.createElement("p");
 document.querySelector(".container").appendChild(warningText);
+
+// add an event listener for input button
 document.getElementById("addTaskBtn").addEventListener("click", addTask);
 
+// call a function to generate random task for the first time
 generateTask();
