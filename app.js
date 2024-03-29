bf22cd34ec218b85221be9425289c63b5cbb5e37
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-03-29 10:06:57 +0700

    feat: add conditions for input

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
 // Place your code here
 // Add any additional code necessary to fulfill the requirements of the assignment
 
-const getTask = async () => {
+let taskId = 0;
+
+const generateTask = async () => {
     const response = await fetch("https://module3-api-is2m.onrender.com/random-todos");
     const data = await response.json();
-    taskData = data;
+    let taskData = data;
 
     if (taskData.length > 0) {
-        generateTaskList();
+        showTaskList(taskData);
     }
 };
 
 const addTask = () => {
     const newTask = document.getElementById("newTask");
     const taskList = document.getElementById("taskList");
+    const isInput = newTask.value.trim();
 
-    //  .insertAdjacentHTML is used to preserve the event listener 
-    //  ref=> https://stackoverflow.com/questions/595808/is-it-possible-to-append-to-innerhtml-without-destroying-descendants-event-list
-    taskList.insertAdjacentHTML("beforeend", `<li id='list-${++taskId}'>${newTask.value}</li>`);
-    addClickListener(taskId);
-}
+    if (isInput) {
+        //  .insertAdjacentHTML is used to preserve the event listener 
+        //  ref=> https://stackoverflow.com/questions/595808/is-it-possible-to-append-to-innerhtml-without-destroying-descendants-event-list
+        taskList.insertAdjacentHTML("beforeend", `<li id='list-${++taskId}'>${newTask.value}</li>`);
+        newTask.value = '';
+        addClickListener(taskId);
+    }
+};
 
 const addClickListener = (index) => {
     let onClickTaskGenerator = onClickTask(index);
     document.getElementById(`list-${index}`).addEventListener("click", () => onClickTaskGenerator.next());
-}
+};
 
 function* onClickTask(index) {
     const clickedTask = document.getElementById(`list-${index}`);
-    console.log("clicked");
     yield clickedTask.classList.add("task-done");
     
     clickedTask.remove();
 }
 
-const generateTaskList = () => {
+const showTaskList = (taskData) => {
     const taskList = document.getElementById("taskList");
 
     for (let i = 0; i < taskData.length; i++) {
         const task = taskData[i];
         taskId = i;
         taskList.insertAdjacentHTML("beforeend", `<li id='list-${taskId}'>${task}</li>`);
-        addClickListener(i)
+        addClickListener(i);
     }
-}
+};
 
 document.getElementById("addTaskBtn").addEventListener("click", addTask);
 
-let taskData = [];
-let taskId = 0;
-getTask();
+generateTask();
